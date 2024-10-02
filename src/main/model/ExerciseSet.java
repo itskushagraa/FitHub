97f1c3931f56570119fde72e4979a6cde9251c71
Kushagra Sharma <ksharm22@student.ubc.ca>
2024-10-01 21:23:50 -0700

    Created and Documented Supplementary Classes and Test Classes: ExerciseSet.java, TestExerciseSet.java

---
+++ src/main/model/ExerciseSet.java
@@ -0,0 +1,46 @@
+package model;
+
+/** 
+ * Represents one set of an exercise
+ * Stores the number of reps and weight lifted in that set
+**/
+
+public class ExerciseSet {
+    private int reps;       // reps completed in the set
+    private int weight;     // weight lifted in the set
+
+    /*
+     * REQUIRES: 
+     *  - reps > 0
+     *  - weight > 0
+     *
+     * EFFECTS: 
+     *  - Initializes this ExerciseSet with the provided reps and weight lifted
+     */
+    public ExerciseSet(int reps, int weight) {
+        // STUB TODO
+    }
+
+    /*
+     * SETTER METHODS: 
+     */
+    public void setReps(int reps) {
+        // STUB TODO
+    }
+
+    public void setWeight(int weight) {
+        // STUB TODO
+    }
+
+
+    /*
+     * GETTER METHODS: 
+     */
+    public int getReps() {
+        return 0; // STUB TODO
+    }
+
+    public int getWeight() {
+        return 0; // STUB TODO
+    }
+}
