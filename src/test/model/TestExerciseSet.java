832a9663f132a2e1fe31a3feb4cc675a2a13f0c9
Kushagra Sharma <ksharm22@student.ubc.ca>
2024-10-01 23:58:15 -0700

    Created Tests: ExerciseSet.java

--- src/test/model/TestExerciseSet.java
+++ src/test/model/TestExerciseSet.java
@@ -1,5 +1,29 @@
 package model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TestExerciseSet {
+    private ExerciseSet testSet;
     
+    @BeforeEach
+    void runBefore() {
+        testSet = new ExerciseSet(10, 100);
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals(10, testSet.getReps());
+        assertEquals(100, testSet.getWeight());
+    }
+
+    @Test
+    void testSetterMethods() {
+        testSet.setReps(5);
+        testSet.setWeight(150);
+        assertEquals(5, testSet.getReps());
+        assertEquals(150, testSet.getWeight());
+    }
 }
