b400d0a3192b719a7e7c2c2dd9a56548596bbafa
Kushagra Sharma <ksharm22@student.ubc.ca>
2024-10-05 19:09:22 -0700

    Created Tests: Meal.java

--- src/test/model/TestMeal.java
+++ src/test/model/TestMeal.java
@@ -1,5 +1,105 @@
 package model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import data.TemporaryData;
+
 public class TestMeal {
-    
+    private Meal testMeal;
+
+    @BeforeEach
+    void runBefore() {
+        testMeal = new Meal(
+            "Spaghetti Bolognese",
+            "Dinner",
+            Arrays.asList("Spaghetti", "Ground Beef", "Tomato Sauce", "Parmesan Cheese"),
+            700,
+            350.0,
+            25.0,
+            30.0,
+            85.0);
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Spaghetti Bolognese", testMeal.getName());
+        assertEquals("Dinner", testMeal.getType());
+        List<String> testIngredients = Arrays.asList("Spaghetti", "Ground Beef", "Tomato Sauce", "Parmesan Cheese");
+        assertEquals(testIngredients, testMeal.getIngredients());
+        assertEquals(700, testMeal.getCalories());
+        assertEquals(350.0, testMeal.getQuantity());
+        assertEquals(25.0, testMeal.getProtein());
+        assertEquals(30.0, testMeal.getFat());
+        assertEquals(85.0, testMeal.getCarb());
+    }
+
+    @Test
+    void testAddSingleIngredient() {
+        assertEquals(4, testMeal.getIngredients().size());
+        testMeal.addIngredient("Salt");
+        assertEquals("Salt", testMeal.getIngredients().get(4));
+        assertEquals(5, testMeal.getIngredients().size());
+    }
+
+    @Test
+    void testAddMultipleIngredient() {
+        assertEquals(4, testMeal.getIngredients().size());
+        testMeal.addIngredient("Salt");
+        testMeal.addIngredient("Pepper");
+        testMeal.addIngredient("Thyme");
+        assertEquals("Salt", testMeal.getIngredients().get(4));
+        assertEquals("Pepper", testMeal.getIngredients().get(5));
+        assertEquals("Thyme", testMeal.getIngredients().get(6));
+        assertEquals(7, testMeal.getIngredients().size());
+    }
+
+    @Test
+    void testRemoveSingleIngredient() {
+        assertEquals(4, testMeal.getIngredients().size());
+        testMeal.removeIngredient("Spaghetti");
+        assertEquals(3, testMeal.getIngredients().size());
+        assertEquals("Ground Beef", testMeal.getIngredients().get(0));
+    }
+
+    @Test
+    void testRemoveMultipleIngredient() {
+        assertEquals(4, testMeal.getIngredients().size());
+        testMeal.removeIngredient("Spaghetti");
+        testMeal.removeIngredient("Ground Beef");
+        testMeal.removeIngredient("Tomato Sauce");
+        testMeal.removeIngredient("Parmesan Cheese");
+        assertEquals(0, testMeal.getIngredients().size());
+    }
+
+    @Test
+    void testCalculateMethods() {
+        assertEquals(2, testMeal.calculateCaloriesPerGram());
+        assertEquals(0.07142857143, testMeal.calculateProteinPerGram());
+        assertEquals(0.08571428571, testMeal.calculateFatPerGram());
+        assertEquals(0.2428571429, testMeal.calculateCarbPerGram());
+    }
+
+    @Test
+    void testSetQuantity() {
+        testMeal.setQuantity(700.0);
+        assertEquals(700.0, testMeal.getQuantity());
+        assertEquals(1400, testMeal.getCalories());
+        assertEquals(50.0, testMeal.getProtein());
+        assertEquals(60.0, testMeal.getFat());
+        assertEquals(170.0, testMeal.getCarb());
+    }
+
+    @Test
+    void testSetterMethods() {
+        testMeal.setName("Omelette");
+        assertEquals("Omelette", testMeal.getName());
+        testMeal.setType("Breakfast");
+        assertEquals("Breakfast", testMeal.getType());
+    }
 }
