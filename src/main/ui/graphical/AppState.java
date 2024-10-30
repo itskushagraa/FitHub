af2a371e8d876ee2754aacba6c696001d9afb9c5
Kushagra Sharma <ksharm22@student.ubc.ca>
2024-10-29 22:17:49 -0700

    Developed State Machine for GUI: AppState.java (Enum)

---
+++ src/main/ui/graphical/AppState.java
@@ -0,0 +1,11 @@
+package ui.graphical;
+
+public enum AppState {
+    SPLASH_SCREEN,
+    WELCOME_SCREEN,
+    USER_SETUP,
+    MAIN_MENU,
+    WORKOUT_TRACKER,
+    DIET_PLANNER, 
+    USER_PROFILE
+}
